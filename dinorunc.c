844264cc3814dd1c7e62294a121df058e0a8c25e
Bruno Moraes <89628890+brunomoraesdigital@users.noreply.github.com>
2026-01-04 22:40:35 -0300

    Add files via upload

---
+++ dinorunc.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main () {
+	
+	int pontuacao = 0;
+	float energia = 100;
+	int nivel = 1;
+	
+	
+	printf("Dino C  Run\n");
+	printf("- - - - - - - - - - - - - - - - -\n");
+	printf("Dino C\n");
+	printf("Nível %d\n", nivel);
+	printf("Energia %.1f\n", energia);
+	printf("Pontuação %d\n", pontuacao);
+	
+	
+	return 0;
+}
